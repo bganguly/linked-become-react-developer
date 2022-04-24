7ab6f6e22b01a603c72f5580c8986f8dd8037d9f
bganguly <gangulybikramjit@hotmail.com>
2022-04-24 14:40:59 -0700

    removed unnecessary div wrapper from githubuser component

--- src/componentsForDataFetch/githubUser.js
+++ src/componentsForDataFetch/githubUser.js
@@ -1,6 +1,6 @@
 const GithubUser = ({user}) => {
   return (
-    <div>
+    <>
       <p>{user.login} </p>
       <p>{user.html_url} </p>
       {user.avatar_url
@@ -11,7 +11,7 @@ const GithubUser = ({user}) => {
             height="200"/>
         </p>
       : null}
-    </div>
+    </>
   )
 }
 
