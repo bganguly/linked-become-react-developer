73e89caa6c40adcbe950592d0de8fb2cba127fda
bganguly <gangulybikramjit@hotmail.com>
2022-04-26 14:05:56 -0700

    demo how to pass props as spread operator

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import './App.css';
 import CheckBoxUsingHooks from './components/checkBoxWithUseReducer';
 import GithubUserList from './componentsForDataFetch/githubUserList';
 import GithubUserById from './componentsForDataFetch/githubUserById';
+import GithubUserUsingSpreadOperator from './componentsForDataFetch/githubUserUsingSpreadOperator';
 
 import StarRating from './components/StarRating';
 
 function App() {
   const user = {'login':'lukas'}
+  const userUsingSpreadOperator = {
+    'login':'lukas', 
+    html_url:'https://github.com/lukas',
+    avatar_url:'https://avatars.githubusercontent.com/u/29?v=4'
+  }
+
   return (
     // < CheckBoxUsingHooks />
     // <GithubUserList />
-    <GithubUserById user={user}/>
+    // <GithubUserById user={user}/>
+    <GithubUserUsingSpreadOperator {...userUsingSpreadOperator}/>
     // <StarRating />
   );
 }
