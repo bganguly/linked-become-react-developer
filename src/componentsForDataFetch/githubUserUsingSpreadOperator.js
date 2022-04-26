4d57c3b590fca25151662761266bfa40ebe5e274
bganguly <gangulybikramjit@hotmail.com>
2022-04-26 15:58:52 -0700

    demo how to pass props as spread operator- use anchor tag for github link

---
+++ src/componentsForDataFetch/githubUserUsingSpreadOperator.js
@@ -0,0 +1,20 @@
+const GithubUserUsingSpreadOperator = ({login, html_url, avatar_url}) => {
+  return (
+    <>
+      <p>{login} </p>
+      <p>
+        <a href={html_url}>github link</a>
+      </p>
+      {avatar_url
+      ? <p>
+          <img src={avatar_url} 
+            alt="not provided" 
+            width="200"
+            height="200"/>
+        </p>
+      : null}
+    </>
+  )
+}
+
+export default GithubUserUsingSpreadOperator;
